2a2a14f438e417bbcaae588d32a624995f7feabb
duanjinqiu <471568720@qq.com>
2024-03-12 23:52:58 +0800

    feat: 添加vue，vue-router，并配置vite

---
+++ src/router/index.ts
@@ -0,0 +1,11 @@
+import Home from "../views/Home.vue";
+import { createRouter, createWebHistory } from 'vue-router';
+const routes = [{
+    path: '/',
+    component: Home
+}];
+
+export default createRouter({
+    history: createWebHistory(),
+    routes
+});
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        我是Home.vue组件
+        <div>
+            <input type="text" v-model="msg">
+        </div>
+        <p>
+            {{ msg }}
+        </p>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const msg = ref('Hello World!');
+</script>
